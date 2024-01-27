787496ec79f6e308fa499410244cda1d7d31de33
Laranjo <tomas.laranjo@ua.pt>
2024-01-27 14:13:37 +0000

    Coding done

---
+++ src/Items_db.gd
@@ -0,0 +1,47 @@
+extends Node
+
+
+const ICON_PATH = "res://Images/Items/Upgrades/"
+const WEAPON_PATH = "res://Images/Items/Weapons/"
+const UPGRADES = {
+	"foxtail1": {
+		"icon": WEAPON_PATH + "fox_tail.png",
+		"displayname": "Fox Tail",
+		"details": "A fox tail appears and strikes in the direction the player is facing",
+		"level": "Level: 1",
+		"prerequisite": [],
+		"type": "weapon"
+	},
+	"foxtail2": {
+		"icon": WEAPON_PATH + "fox_tail.png",
+		"displayname": "Fox Tail",
+		"details": "A fox tail appears and strikes in the direction the player is facing",
+		"level": "Level: 2",
+		"prerequisite": ["foxtail1"],
+		"type": "weapon"
+	},
+	"foxtail3": {
+		"icon": WEAPON_PATH + "fox_tail.png",
+		"displayname": "Fox Tail",
+		"details": "A fox tail appears and strikes in the direction the player is facing",
+		"level": "Level: 3",
+		"prerequisite": ["foxtail2"],
+		"type": "weapon"
+	},
+	"foxtail4": {
+		"icon": WEAPON_PATH + "fox_tail.png",
+		"displayname": "Fox Tail",
+		"details": "A fox tail appears and strikes in the direction the player is facing",
+		"level": "Level: 4",
+		"prerequisite": ["foxtail3"],
+		"type": "weapon"
+	},
+	"foxtail5": {
+		"icon": WEAPON_PATH + "fox_tail.png",
+		"displayname": "Fox Tail",
+		"details": "A fox tail appears and strikes in the direction the player is facing",
+		"level": "Level: 5",
+		"prerequisite": ["foxtail4"],
+		"type": "weapon"
+	},
+	}
