da989b3ca0d852566fcd922c0eb9c062662bdfb9
Laranjo <tomas.laranjo@ua.pt>
2024-01-28 00:57:13 +0000

    swipe and stuff

--- Player/Attack/swipe.gd
+++ Player/Attack/swipe.gd
@@ -3,18 +3,21 @@ extends Area2D
 var level = 1
 var hp = 1
 var speed = 100
-var damage = 5
+var damage = 10
+var attack_speed = 2.0
+var attack_range = 100
 var knockback_amount = 100
 var attack_size = 1.0
 
+var last_movement = Vector2.ZERO
 var target = Vector2.ZERO
 var angle = Vector2.ZERO
 
-@onready var player = get_tree().get_first_node_in_group("player")
-signal remove_from_array(object)
+@onready var player = get_node("/root/Player") # Adjust the path according to your player node
+signal remove_from_array
 
 func _ready():
-	angle = global_position.direction_to(target)
+	angle = player.global_position.direction_to(player.global_position + player.last_movement) # Adjust the angle based on player's direction
 	rotation = angle.angle() + deg_to_rad(135)
 	match level:
 		1:
@@ -41,14 +44,19 @@ func _ready():
 			damage = 8
 			knockback_amount = 100
 			attack_size = 1.0 * (1 + player.spell_size)
-
+		5:
+			hp = 2
+			speed = 120
+			damage = 10
+			knockback_amount = 100
+			attack_size = 1.0 * (1 + player.spell_size)
 	
 	var tween = create_tween()
-	tween.tween_property(self,"scale",Vector2(1,1)*attack_size,1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
-	tween.play()
+	tween.tween_property(self, "scale", Vector2(1, 1) * attack_size, 1).set_trans(Tween.TRANS_QUINT).set_ease(Tween.EASE_OUT)
+	tween.start()
 
 func _physics_process(delta):
-	position += angle*speed*delta
+	position += player.global_position + player.last_movement.normalized() * attack_range
 
 func enemy_hit(charge = 1):
 	hp -= charge
@@ -57,5 +65,5 @@ func enemy_hit(charge = 1):
 		queue_free()
 
 func _on_timer_timeout():
-	emit_signal("remove_from_array",self)
+	emit_signal("remove_from_array", self)
 	queue_free()
--- Player/player.gd
+++ Player/player.gd
@@ -15,6 +15,7 @@ var collected_experience = 0
 var iceSpear = preload("res://Player/Attack/ice_spear.tscn")
 var tornado = preload("res://Player/Attack/tornado.tscn")
 var javelin = preload("res://Player/Attack/javelin.tscn")
+var swipe = preload("res://Player/Attack/swipe.tscn")
 
 #AttackNodes
 @onready var iceSpearTimer = get_node("%IceSpearTimer")
@@ -22,6 +23,8 @@ var javelin = preload("res://Player/Attack/javelin.tscn")
 @onready var tornadoTimer = get_node("%TornadoTimer")
 @onready var tornadoAttackTimer = get_node("%TornadoAttackTimer")
 @onready var javelinBase = get_node("%JavelinBase")
+@onready var swipeTimer = get_node("%SwipeTimer")
+@onready var swipeAttackTimer = get_node("%SwipeAttackTimer")
 
 #UPGRADES
 var collected_upgrades = []
@@ -48,6 +51,12 @@ var tornado_level = 0
 var javelin_ammo = 0
 var javelin_level = 0
 
+#swipe
+var swipe_ammo = 0
+var swipe_baseammo = 0
+var swipe_attackspeed = 1.5
+var swipe_level = 0
+var swipe_attackrange = 100
 
 #Enemy Related
 var enemy_close = []
@@ -78,7 +87,7 @@ var enemy_close = []
 signal playerdeath
 
 func _ready():
-	upgrade_character("icespear1")
+	upgrade_character("swipe1")
 	attack()
 	set_expbar(experience, calculate_experiencecap())
 	_on_hurt_box_hurt(0,0,0)
@@ -118,7 +127,11 @@ func attack():
 			tornadoTimer.start()
 	if javelin_level > 0:
 		spawn_javelin()
-
+	if swipe_level > 0:
+		swipeTimer.wait_time = swipe_attackspeed * (1-spell_cooldown)
+		if swipeTimer.is_stopped():
+			swipeTimer.start() 
+		
 func _on_hurt_box_hurt(damage, _angle, _knockback):
 	hp -= clamp(damage-armor, 1.0, 999.0)
 	healthBar.max_value = maxhp
@@ -174,6 +187,30 @@ func spawn_javelin():
 	for i in get_javelins:
 		if i.has_method("update_javelin"):
 			i.update_javelin()
+
+func _on_swipe_timer_timeout():
+	# Reduce swipe ammo and start the attack timer
+	swipe_ammo += swipe_baseammo + additional_attacks
+	swipeAttackTimer.start()
+
+func _on_swipe_attack_timer_timeout():
+	# Check if there's ammo available
+	if swipe_ammo > 0:
+		# Instantiate the swipe attack
+		var swipe_attack = swipe.instantiate()
+		# Set the position and direction of the swipe attack
+		swipe_attack.position = position * swipe_attackrange
+		swipe_attack.last_movement = last_movement
+		swipe_attack.level = swipe_level  # Assuming swipe_level is declared elsewhere
+		# Add the swipe attack as a child
+		add_child(swipe_attack)
+		# Reduce swipe ammo
+		swipe_ammo -= 1
+		# Check if there's more ammo left to continue attacking
+		if swipe_ammo > 0:
+			swipeAttackTimer.start()
+		else:
+			swipeAttackTimer.stop()
 
 func get_random_target():
 	if enemy_close.size() > 0:
@@ -280,6 +317,15 @@ func upgrade_character(upgrade):
 			javelin_level = 3
 		"javelin4":
 			javelin_level = 4
+		"swipe1":
+			swipe_level = 1
+			swipe_ammo = 1
+		"swipe2":
+			swipe_level = 2
+		"swipe3":
+			swipe_level = 3
+		"swipe4":
+			swipe_level = 4
 		"armor1","armor2","armor3","armor4":
 			armor += 1
 		"speed1","speed2","speed3","speed4":
