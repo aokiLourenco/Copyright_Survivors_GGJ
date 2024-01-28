030538b304badbf4dd1d8536158d2e6e6d0ce09b
Anderson Aoki Lourenço <aaokilourenco@ua.pt>
2024-01-28 03:43:03 +0000

    asdfas

--- Utility/upgrade_db.gd
+++ Utility/upgrade_db.gd
@@ -1,256 +1,388 @@
 extends Node
 
 
+const UPGRADE_PATH = "res://images/items/Upgrades/"
+const EXP_PATH = "res://images/items/Experience/"
+const CHEST_PATH = "res://images/items/Chest/"
+
 const ICON_PATH = "res://Textures/Items/Upgrades/"
 const WEAPON_PATH = "res://Textures/Items/Weapons/"
 const UPGRADES = {
-	"icespear1": {
-		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "A spear of ice is thrown at a random enemy",
-		"level": "Level: 1",
-		"prerequisite": [],
-		"type": "weapon"
-	},
-	"icespear2": {
-		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "An addition Ice Spear is thrown",
-		"level": "Level: 2",
-		"prerequisite": ["icespear1"],
-		"type": "weapon"
-	},
-	"icespear3": {
-		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "Ice Spears now pass through another enemy and do + 3 damage",
-		"level": "Level: 3",
-		"prerequisite": ["icespear2"],
-		"type": "weapon"
-	},
-	"icespear4": {
-		"icon": WEAPON_PATH + "ice_spear.png",
-		"displayname": "Ice Spear",
-		"details": "An additional 2 Ice Spears are thrown",
-		"level": "Level: 4",
-		"prerequisite": ["icespear3"],
-		"type": "weapon"
-	},
 	"javelin1": {
-		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "A magical javelin will follow you attacking enemies in a straight line",
-		"level": "Level: 1",
+		"icon": WEAPON_PATH + "onion.png",
+		"displayname": "javelin",
+		"details": "A wise ogre once said: “Life is  like onions, it has many LAYERS!” Plus you can keep people away with your bad breath.",
+		"level": "Level 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
-	"javelin2": {
-		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin will now attack an additional enemy per attack",
-		"level": "Level: 2",
-		"prerequisite": ["javelin1"],
-		"type": "weapon"
-	},
-	"javelin3": {
-		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin will attack another additional enemy per attack",
-		"level": "Level: 3",
-		"prerequisite": ["javelin2"],
-		"type": "weapon"
-	},
-	"javelin4": {
-		"icon": WEAPON_PATH + "javelin_3_new_attack.png",
-		"displayname": "Javelin",
-		"details": "The javelin now does + 5 damage per attack and causes 20% additional knockback",
-		"level": "Level: 4",
-		"prerequisite": ["javelin3"],
-		"type": "weapon"
-	},
-	"tornado1": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "A tornado is created and random heads somewhere in the players direction",
-		"level": "Level: 1",
+	"onion1": {
+		"icon": WEAPON_PATH + "onion.png",
+		"displayname": "Onion",
+		"details": "A wise ogre once said: “Life is  like onions, it has many LAYERS!” Plus you can keep people away with your bad breath.",
+		"level": "Level 1",
 		"prerequisite": [],
 		"type": "weapon"
 	},
-	"tornado2": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "An additional Tornado is created",
-		"level": "Level: 2",
-		"prerequisite": ["tornado1"],
+	"onion2": {
+		"icon": WEAPON_PATH + "onion.png",
+		"displayname": "Onion",
+		"details": "A wise ogre once said: “Life is  like onions, it has many LAYERS!” Plus you can keep people away with your bad breath.",
+		"level": "Level 2",
+		"prerequisite": ["onion1"],
 		"type": "weapon"
 	},
-	"tornado3": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "The Tornado cooldown is reduced by 0.5 seconds",
-		"level": "Level: 3",
-		"prerequisite": ["tornado2"],
+	"onion3": {
+		"icon": WEAPON_PATH + "onion.png",
+		"displayname": "Onion",
+		"details": "A wise ogre once said: “Life is  like onions, it has many LAYERS!” Plus you can keep people away with your bad breath.",
+		"level": "Level 3",
+		"prerequisite": ["onion2"],
 		"type": "weapon"
 	},
-	"tornado4": {
-		"icon": WEAPON_PATH + "tornado.png",
-		"displayname": "Tornado",
-		"details": "An additional tornado is created and the knockback is increased by 25%",
-		"level": "Level: 4",
-		"prerequisite": ["tornado3"],
+	"onion4": {
+		"icon": WEAPON_PATH + "onion.png",
+		"displayname": "Onion",
+		"details": "A wise ogre once said: “Life is  like onions, it has many LAYERS!” Plus you can keep people away with your bad breath.",
+		"level": "Level 4",
+		"prerequisite": ["onion3"],
 		"type": "weapon"
 	},
-	"armor1": {
-		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By 1 point",
-		"level": "Level: 1",
+	"onion5": {
+		"icon": WEAPON_PATH + "onion.png",
+		"displayname": "Onion",
+		"details": "A wise ogre once said: “Life is  like onions, it has many LAYERS!” Plus you can keep people away with your bad breath.",
+		"level": "Level 5",
+		"prerequisite": ["onion4"],
+		"type": "weapon"
+	},
+	"oven1": {
+		"icon": WEAPON_PATH + "oven.png",
+		"displayname": "Oven",
+		"details": "Just a completely normal household appliance.",
+		"level": "Level 1",
+		"prerequisite": [],
+		"type": "weapon"
+	},
+	"oven2": {
+		"icon": WEAPON_PATH + "oven.png",
+		"displayname": "Oven",
+		"details": "Just a completely normal household appliance.",
+		"level": "Level 2",
+		"prerequisite": ["oven1"],
+		"type": "weapon"
+	},
+	"oven3": {
+		"icon": WEAPON_PATH + "oven.png",
+		"displayname": "Oven",
+		"details": "Just a completely normal household appliance.",
+		"level": "Level 3",
+		"prerequisite": ["oven2"],
+		"type": "weapon"
+	},
+	"oven4": {
+		"icon": WEAPON_PATH + "oven.png",
+		"displayname": "Oven",
+		"details": "Just a completely normal household appliance.",
+		"level": "Level 4",
+		"prerequisite": ["oven3"],
+		"type": "weapon"
+	},
+	"oven5": {
+		"icon": WEAPON_PATH + "oven.png",
+		"displayname": "Oven",
+		"details": "Just a completely normal household appliance.",
+		"level": "Level 5",
+		"prerequisite": ["oven4"],
+		"type": "weapon"
+	},
+	"copystrike1": {
+		"icon": WEAPON_PATH + "copystr.png",
+		"displayname": "Copystrike",
+		"details": "What this game would get if it was a Youtube video. Please don’t sue us N*ntendo.",
+		"level": "Level 1",
+		"prerequisite": [],
+		"type": "weapon"
+	},
+	"copystrike2": {
+		"icon": WEAPON_PATH + "copystr.png",
+		"displayname": "Copystrike",
+		"details": "What this game would get if it was a Youtube video. Please don’t sue us N*ntendo.",
+		"level": "Level 2",
+		"prerequisite": ["copystrike1"],
+		"type": "weapon"
+	},
+	"copystrike3": {
+		"icon": WEAPON_PATH + "copystr.png",
+		"displayname": "Copystrike",
+		"details": "What this game would get if it was a Youtube video. Please don’t sue us N*ntendo.",
+		"level": "Level 3",
+		"prerequisite": ["copystrike2"],
+		"type": "weapon"
+	},
+	"copystrike4": {
+		"icon": WEAPON_PATH + "copystr.png",
+		"displayname": "Copystrike",
+		"details": "What this game would get if it was a Youtube video. Please don’t sue us N*ntendo.",
+		"level": "Level 4",
+		"prerequisite": ["copystrike3"],
+		"type": "weapon"
+	},
+	"copystrike5": {
+		"icon": WEAPON_PATH + "copystr.png",
+		"displayname": "Copystrike",
+		"details": "What this game would get if it was a Youtube video. Please don’t sue us N*ntendo.",
+		"level": "Level 5",
+		"prerequisite": ["copystrike4"],
+		"type": "weapon"
+	},
+	"toktik1": {
+		"icon": WEAPON_PATH + "toktik.png",
+		"displayname": "Tok Tik",
+		"details": "It’ll make you swipe more than a desperate person on Tinder. That’s you. You’re the desperate person.",
+		"level": "Level 1",
+		"prerequisite": [],
+		"type": "weapon"
+	},
+	"toktik2": {
+		"icon": WEAPON_PATH + "toktik.png",
+		"displayname": "Tok Tik",
+		"details": "It’ll make you swipe more than a desperate person on Tinder. That’s you. You’re the desperate person.",
+		"level": "Level 2",
+		"prerequisite": ["toktik1"],
+		"type": "weapon"
+	},
+	"toktik3": {
+		"icon": WEAPON_PATH + "toktik.png",
+		"displayname": "Tok Tik",
+		"details": "It’ll make you swipe more than a desperate person on Tinder. That’s you. You’re the desperate person.",
+		"level": "Level 3",
+		"prerequisite": ["toktik2"],
+		"type": "weapon"
+	},
+	"toktik4": {
+		"icon": WEAPON_PATH + "toktik.png",
+		"displayname": "Tok Tik",
+		"details": "It’ll make you swipe more than a desperate person on Tinder. That’s you. You’re the desperate person.",
+		"level": "Level 4",
+		"prerequisite": ["toktik3"],
+		"type": "weapon"
+	},
+	"toktik5": {
+		"icon": WEAPON_PATH + "toktik.png",
+		"displayname": "Tok Tik",
+		"details": " It’ll make you swipe more than a desperate person on Tinder. That’s you. You’re the desperate person.",
+		"level": "Level 5",
+		"prerequisite": ["toktik4"],
+		"type": "weapon"
+	},
+	"moreram1": {
+		"icon": UPGRADE_PATH + "moreram.png",
+		"displayname": "Download RAM",
+		"details": "I swear, this is not a virus. (Sweats profusely)",
+		"level": "Level 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
-	"armor2": {
-		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
-		"level": "Level: 2",
-		"prerequisite": ["armor1"],
+	"moreram2": {
+		"icon": UPGRADE_PATH + "moreram.png",
+		"displayname": "Download RAM",
+		"details": "I swear, this is not a virus. (Sweats profusely)",
+		"level": "Level 2",
+		"prerequisite": ["moreram1"],
 		"type": "upgrade"
 	},
-	"armor3": {
-		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
-		"level": "Level: 3",
-		"prerequisite": ["armor2"],
+	"moreram3": {
+		"icon": UPGRADE_PATH + "moreram.png",
+		"displayname": "Download RAM",
+		"details": "I swear, this is not a virus. (Sweats profusely)",
+		"level": "Level 3",
+		"prerequisite": ["moreram2"],
 		"type": "upgrade"
 	},
-	"armor4": {
-		"icon": ICON_PATH + "helmet_1.png",
-		"displayname": "Armor",
-		"details": "Reduces Damage By an additional 1 point",
-		"level": "Level: 4",
-		"prerequisite": ["armor3"],
+	"moreram4": {
+		"icon": UPGRADE_PATH + "moreram.png",
+		"displayname": "Download RAM",
+		"details": "I swear, this is not a virus. (Sweats profusely)",
+		"level": "Level 4",
+		"prerequisite": ["moreram3"],
 		"type": "upgrade"
 	},
-	"speed1": {
-		"icon": ICON_PATH + "boots_4_green.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased by 50% of base speed",
-		"level": "Level: 1",
+	"moreram5": {
+		"icon": UPGRADE_PATH + "moreram.png",
+		"displayname": "Download RAM",
+		"details": "I swear, this is not a virus. (Sweats profusely)",
+		"level": "Level 5",
+		"prerequisite": ["moreram4"],
+		"type": "upgrade"
+	},
+	"energydrink1": {
+		"icon": UPGRADE_PATH + "green_cow.png",
+		"displayname": "Energy Drink",
+		"details": "Overclocking in real life. Side effects included.",
+		"level": "Level 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
-	"speed2": {
-		"icon": ICON_PATH + "boots_4_green.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased by an additional 50% of base speed",
-		"level": "Level: 2",
-		"prerequisite": ["speed1"],
+	"energydrink2": {
+		"icon": UPGRADE_PATH + "green_cow.png",
+		"displayname": "Energy Drink",
+		"details": "Overclocking in real life. Side effects included.",
+		"level": "Level 2",
+		"prerequisite": ["energydrink1"],
 		"type": "upgrade"
 	},
-	"speed3": {
-		"icon": ICON_PATH + "boots_4_green.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased by an additional 50% of base speed",
-		"level": "Level: 3",
-		"prerequisite": ["speed2"],
+	"energydrink3": {
+		"icon": UPGRADE_PATH + "green_cow.png",
+		"displayname": "Energy Drink",
+		"details": "Overclocking in real life. Side effects included.",
+		"level": "Level 3",
+		"prerequisite": ["energydrink2"],
 		"type": "upgrade"
 	},
-	"speed4": {
-		"icon": ICON_PATH + "boots_4_green.png",
-		"displayname": "Speed",
-		"details": "Movement Speed Increased an additional 50% of base speed",
-		"level": "Level: 4",
-		"prerequisite": ["speed3"],
+	"energydrink4": {
+		"icon": UPGRADE_PATH + "green_cow.png",
+		"displayname": "Energy Drink",
+		"details": "Overclocking in real life. Side effects included.",
+		"level": "Level 4",
+		"prerequisite": ["energydrink3"],
 		"type": "upgrade"
 	},
-	"tome1": {
-		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
-		"level": "Level: 1",
+	"energydrink5": {
+		"icon": UPGRADE_PATH + "green_cow.png",
+		"displayname": "Energy Drink",
+		"details": "Overclocking in real life. Side effects included.",
+		"level": "Level 5",
+		"prerequisite": ["energydrink4"],
+		"type": "upgrade"
+	},
+	"deadline1": {
+		"icon": UPGRADE_PATH + "calendar.png",
+		"displayname": "Deadline",
+		"details": "You’re twice as productive if you don’t sleep. I haven’t known sleep in 48 hours.",
+		"level": "Level 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
-	"tome2": {
-		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
-		"level": "Level: 2",
-		"prerequisite": ["tome1"],
+	"deadline2": {
+		"icon": UPGRADE_PATH + "calendar.png",
+		"displayname": "Deadline",
+		"details": "You’re twice as productive if you don’t sleep. I haven’t known sleep in 48 hours.",
+		"level": "Level 2",
+		"prerequisite": ["deadline1"],
 		"type": "upgrade"
 	},
-	"tome3": {
-		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
-		"level": "Level: 3",
-		"prerequisite": ["tome2"],
+	"deadline3": {
+		"icon": UPGRADE_PATH + "calendar.png",
+		"displayname": "Deadline",
+		"details": "You’re twice as productive if you don’t sleep. I haven’t known sleep in 48 hours.",
+		"level": "Level 3",
+		"prerequisite": ["deadline2"],
 		"type": "upgrade"
 	},
-	"tome4": {
-		"icon": ICON_PATH + "thick_new.png",
-		"displayname": "Tome",
-		"details": "Increases the size of spells an additional 10% of their base size",
-		"level": "Level: 4",
-		"prerequisite": ["tome3"],
+	"deadline4": {
+		"icon": UPGRADE_PATH + "calendar.png",
+		"displayname": "Deadline",
+		"details": "You’re twice as productive if you don’t sleep. I haven’t known sleep in 48 hours.",
+		"level": "Level 4",
+		"prerequisite": ["deadline3"],
 		"type": "upgrade"
 	},
-	"scroll1": {
-		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
-		"level": "Level: 1",
+	"deadline5": {
+		"icon": UPGRADE_PATH + "calendar.png",
+		"displayname": "Deadline",
+		"details": "You’re twice as productive if you don’t sleep. I haven’t known sleep in 48 hours.",
+		"level": "Level 5",
+		"prerequisite": ["deadline4"],
+		"type": "upgrade"
+	},
+	"direx1": {
+		"icon": UPGRADE_PATH + "direx.png",
+		"displayname": "Direx",
+		"details": "Remember to use protection, not like you’ll ever need it...",
+		"level": "Level 1",
 		"prerequisite": [],
 		"type": "upgrade"
 	},
-	"scroll2": {
-		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
-		"level": "Level: 2",
-		"prerequisite": ["scroll1"],
+	"direx2": {
+		"icon": UPGRADE_PATH + "direx.png",
+		"displayname": "Direx",
+		"details": "Remember to use protection, not like you’ll ever need it...",
+		"level": "Level 2",
+		"prerequisite": ["direx1"],
 		"type": "upgrade"
 	},
-	"scroll3": {
-		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
-		"level": "Level: 3",
-		"prerequisite": ["scroll2"],
+	"direx3": {
+		"icon": UPGRADE_PATH + "direx.png",
+		"displayname": "Direx",
+		"details": "Remember to use protection, not like you’ll ever need it...",
+		"level": "Level 3",
+		"prerequisite": ["direx2"],
 		"type": "upgrade"
 	},
-	"scroll4": {
-		"icon": ICON_PATH + "scroll_old.png",
-		"displayname": "Scroll",
-		"details": "Decreases of the cooldown of spells by an additional 5% of their base time",
-		"level": "Level: 4",
-		"prerequisite": ["scroll3"],
+	"direx4": {
+		"icon": UPGRADE_PATH + "direx.png",
+		"displayname": "Direx",
+		"details": "Remember to use protection, not like you’ll ever need it...",
+		"level": "Level 4",
+		"prerequisite": ["direx3"],
 		"type": "upgrade"
 	},
-	"ring1": {
-		"icon": ICON_PATH + "urand_mage.png",
-		"displayname": "Ring",
-		"details": "Your spells now spawn 1 more additional attack",
-		"level": "Level: 1",
+	"direx5": {
+		"icon": UPGRADE_PATH + "direx.png",
+		"displayname": "Direx",
+		"details": "Remember to use protection, not like you’ll ever need it...",
+		"level": "Level 5",
+		"prerequisite": ["direx4"],
+		"type": "upgrade"
+	},
+	"techsup": {
+		"icon": EXP_PATH + "techsupport.png",
+		"displayname": "Tech Support",
+		"details": "Have you tried turning it off and on? It’ll fix most things, even body parts.",
+		"level": "Level 0",
 		"prerequisite": [],
-		"type": "upgrade"
+		"type": "exp"
 	},
-	"ring2": {
-		"icon": ICON_PATH + "urand_mage.png",
-		"displayname": "Ring",
-		"details": "Your spells now spawn an additional attack",
-		"level": "Level: 2",
-		"prerequisite": ["ring1"],
-		"type": "upgrade"
-	},
-	"food": {
-		"icon": ICON_PATH + "chunk.png",
-		"displayname": "Food",
-		"details": "Heals you for 20 health",
-		"level": "N/A",
+	"exp1": {
+		"icon": EXP_PATH + "Floppy_Dick1.png",
+		"displayname": "Floppy Disk",
+		"details": "A relic from ancient times. You’re more knowledgeable.",
+		"level": "Level 1",
 		"prerequisite": [],
-		"type": "item"
+		"type": "exp"
+	},
+	"exp2": {
+		"icon": EXP_PATH + "Floppy_Dick2.png",
+		"displayname": "Floppy Disk",
+		"details": "A relic from ancient times. You’re more knowledgeable.",
+		"level": "Level 2",
+		"prerequisite": [],
+		"type": "exp"
+	},
+	"exp3": {
+		"icon": EXP_PATH + "Floppy_Dick3.png",
+		"displayname": "Floppy Disk",
+		"details": "A relic from ancient times. You’re more knowledgeable.",
+		"level": "Level 3",
+		"prerequisite": [],
+		"type": "exp"
+	},
+	"chest": {
+		"icon": CHEST_PATH + "homework.png",
+		"displayname": "'Homework' Folder",
+		"details": "Definitely has items inside.",
+		"level": "Level 1",
+		"prerequisite": [],
+		"type": "chest"
+	},
+	"bluescreen": {
+		"icon": EXP_PATH + "blue-screen.png",
+		"displayname": "Blue Screen",
+		"details": "Oops, there goes all your data. At least it clears the screen.",
+		"level": "Level 0",
+		"prerequisite": [],
+		"type": "exp"
 	}
 }
 
